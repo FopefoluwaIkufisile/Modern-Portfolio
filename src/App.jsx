bc0385466898e6fa0ac91d127c0649efb62d32db
Ikufisile Fopefoluwa <fope.ikufisile@gmail.com>
2025-05-02 03:46:47 -0500

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/Modern-Porfolio" element={<Home />} />
           <Route path="*" element={<NotFound />} />
           <Route />
         </Routes>
