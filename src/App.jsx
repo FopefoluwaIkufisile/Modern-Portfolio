3461e2308fc39dc3eabc85f2570009ff34295191
Ikufisile Fopefoluwa <fope.ikufisile@gmail.com>
2025-05-02 03:41:55 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import { Toaster } from "./components/ui/toaster";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Toaster />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+          <Route />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { ArrowDown } from "lucide-react";
+
+const HeroSection = () => {
+  return (
+    <section
+      id="hero"
+      className="relative min-h-screen flex flex-col items-center justify-center px-4"
+    >
+      <div className="container max-w-4xl mx-auto text-center z-10">
+        <div className="space-y-13">
+          <h1 className="text-4xl md:text-6xl font-bold tracking-tight">
+            <span className="opacity-0 animate-fade-in">Hi, I'm</span>
+            <span className="text-primary opacity-0 animate-fade-in-delay-1">
+              {" "}
+              Fopefoluwa
+            </span>
+            <span className="text-gradient ml-2 opacity-0 animate-fade-in-delay-2">
+              {" "}
+              Ikufisile
+            </span>
+          </h1>
+
+          <p className="text-lg md:text-xl text-muted-foreground max-2-2xl mx-auto opacity-0 animate-fade-in-delay-3">
+          I'm a full-stack web developer with 4+ years of experience designing and developing responsive, high-performance web applications. I specialize in building scalable solutions using modern JavaScript frameworks, RESTful API, etc. I love solving problems with clean code, intuitive UI, and efficient backend architecture.</p>
+          <div className="pt-4 opacity-0 animate-fade-in-delay-4">
+            <a href="#projects" className="cosmic-button">
+              View My Work
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 flex flex-col items-center animate-bounce">
+        <span className="text-sm text-muted-foreground mb-2"> Scroll </span>
+        <ArrowDown className="h-5 w-5 text-primary" />
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
